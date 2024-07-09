ac44d2dd833f9e5785a3a4e2f8bbbe3dd6415d82
Lili Sazonova <lilissazonova@gmail.com>
2024-07-09 19:05:57 +0300

    add search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,21 @@ import SearchBox from './SearchBox/SearchBox';
 import ContactList from './ContactList/ContactList';
 import contacts from './contacts.json';
 import s from './App.module.css';
-// import { useState } from 'react';
+import { useState } from 'react';
 
 const App = () => {
   // const [contacts, setContacts] = useState(contactsData);
+  const [search, setSearch] = useState('');
 
+  const searchContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(search.toLowerCase()),
+  );
   return (
     <div>
       <h1 className={s.title}>Phonebook</h1>
       <ContactForm />
-      <SearchBox />
-      <ContactList contacts={contacts} />
+      <SearchBox value={search} onSearch={setSearch} />
+      <ContactList contacts={searchContacts} />
     </div>
   );
 };
