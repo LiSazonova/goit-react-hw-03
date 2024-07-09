a22f876804e547750d267f46b53f460e7ae070b4
Lili Sazonova <lilissazonova@gmail.com>
2024-07-09 22:23:40 +0300

    add form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,7 +1,18 @@
 import s from './ContactForm.module.css';
+import { Formik, Form, Field } from 'formik';
 
 const ContactForm = () => {
-  return <div></div>;
+  return (
+    <Formik initialValues={{}} onSubmit={() => {}}>
+      <Form className={s.form}>
+        <Field className={s.field} type="text" name="username" />
+        <Field className={s.field} type="email" name="email" />
+        <button className={s.btn} type="submit">
+          Submit
+        </button>
+      </Form>
+    </Formik>
+  );
 };
 
 export default ContactForm;
